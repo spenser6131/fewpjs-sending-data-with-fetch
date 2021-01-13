d7556955db88b4d1b141f5d9df011d4b193243ea
spenser6131 <brinkman.spenser@gmail.com>
2021-01-12 20:10:05 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Add your code here
+function submitData(name, email) {  
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name, email})
+  })
+  .then(resp => resp.json())
+  .then(results => document.body.innerHTML = results["id"])
+  .catch(err => document.body.innerHTML = err.message)
+}
